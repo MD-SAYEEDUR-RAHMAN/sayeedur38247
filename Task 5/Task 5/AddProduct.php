d07c2a3ec7cb7e50ebe7e7ea1c62990246ba7686
MD-SAYEEDUR-RAHMAN <72878224+MD-SAYEEDUR-RAHMAN@users.noreply.github.com>
2021-08-18 17:38:07 +0600

    Add files via upload

---
+++ Task 5/Task 5/AddProduct.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Add Product</title>
+</head>
+<body>
+<form method="post" action="connection.php">
+<fieldset style="width: 370px;">
+  <legend><b>LOGIN</b></legend>
+  Product Name:<br> <input type="text" name="pname"required><br><br>
+  Buying Price:<br> <input type="number" name="bprice"required><br><br>
+  Selling Price:<br> <input type="number" name="sprice"required><br><br>
+
+  <input type="submit" name="submit" value= "Save">
+</fieldset>
+</form>
+</body>
+</html>
